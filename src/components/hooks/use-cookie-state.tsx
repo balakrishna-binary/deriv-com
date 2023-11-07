0ea988100c9e19fd1b81b0be7e9772c589b995ab
balakrishna-binary <bala.krishna@regentmarkets.com>
2023-11-07 11:58:13 +0800

    chore: update log

--- src/components/hooks/use-cookie-state.tsx
+++ src/components/hooks/use-cookie-state.tsx
@@ -9,7 +9,7 @@ export const useCookieState = (key: string, options: { expires: Date }) => {
         return sticky_value ? parseJSONString(sticky_value) : null
     })
 
-    console.log('test17')
+    console.log('test18')
 
     useEffect(() => {
         if (isNullUndefined(value)) {
